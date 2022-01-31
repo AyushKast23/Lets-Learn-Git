7c125230be1ba6128fb5b3c797f12ea5a36d2269
Vidhi Shah <78257771+vshah-21@users.noreply.github.com>
2022-02-01 00:52:59 +0530

    Update multiply.java

--- multiply.java
+++ multiply.java
@@ -1,9 +1,9 @@
 public class Multiply{
-
+    //trial of pull request
     Run|Debug
     public static void main(String[] args){
-        int b = 4;
-        int a = 2;
+        int b = 12;
+        int a = 90;
         System.out.println"The product is " + (a*b);
     }
 }
