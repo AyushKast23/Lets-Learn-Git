ac16e32af216ea2f04ec25dfbe7b3df6baec79dd
AyushKast23 <98771609+AyushKast23@users.noreply.github.com>
2022-02-01 00:33:31 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,8 +2,8 @@ public class Sum{
 
     Run|Debug
     public static void main(String[] args){
-        int b = 45;
-        int a = 5;
+        int b = 20;
+        int a = 10;
         System.out.println("The sum is " + (a+b));
     }
 }
