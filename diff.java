10eb415df3e5c9da42a1f096afd0852960b6eb0b
BHUMIL DAVE <bhumildave91@gmail.com>
2022-02-01 00:23:57 +0530

    Update diff.java

--- diff.java
+++ diff.java
@@ -1,8 +1,8 @@
 public class diff{
     Run|Debug
     public static void main(String[] args){
-        int a=16;
-        int b=15;
+        int a=45;
+        int b=10;
         System.out.println("The difference is " + (a-b));
     }
 }
