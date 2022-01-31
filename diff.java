963df8b265d3ea091cfeed4045b01e7ef4711b01
Vidhi Shah <78257771+vshah-21@users.noreply.github.com>
2022-02-01 00:14:06 +0530

    Update diff.java

--- diff.java
+++ diff.java
@@ -1,3 +1,8 @@
 public class diff{
     Run|Debug
     public static void main(String[] args){
+        int a=9;
+        int b=10;
+        System.out.println("The difference is " + (a-b));
+    }
+}
